48b767e3b4ac0b2caf4db2cbb4c01bb73759d064
LossyDragon <thedragon1012@gmail.com>
2024-03-01 00:49:39 -0600

    clean up imports

--- app/src/main/java/com/example/libxmpoboe/MainActivity.kt
+++ app/src/main/java/com/example/libxmpoboe/MainActivity.kt
@@ -10,7 +10,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -26,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.libxmpoboe.ui.theme.AppTheme
 import kotlin.concurrent.thread
 
